460044b914899c94b29a88016a820523ade3b4f9
Jaine <jaine2005kohn@gmail.com>
2022-12-19 13:45:27 -0300

    adicionar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] idade = {19, 20, 17, 25, 4};
+        String[] nome = {"José", "Jair", "Jaíne", "Julia", "Jordan"};
+        int menorIdade = 1000;
+        int maiorIdade = 0;
+        int media = 0;
+        String nomeIdadeMenor = "";
+        String nomeIdadeMaior = "";
+
+        exercicio3 ex3 = new exercicio3();
+
+        ex3.mostrarPessoas(nome, idade);
+        ex3.descobrirMenor(idade, maiorIdade, nomeIdadeMenor, nome);
+        ex3.descobrirMaior(idade,nomeIdadeMaior,nome);
+        System.out.printf("\n Quem tem a menor idade é %s com %d anos e quem tem a maior idade é %s com %d anos." +
+                nomeIdadeMenor, menorIdade, nomeIdadeMaior, maiorIdade);
     }
+
 }
--- src/exercicio3.java
+++ src/exercicio3.java
@@ -1,20 +1,11 @@
 public class exercicio3 {
-    public static void main(String[] args) {
-        int[] idade = {19, 20, 17, 25, 4};
-        String[] nome = {"José", "Jair", "Jaíne", "Julia", "Jordan"};
-        int menorIdade = 1000;
-        int maiorIdade = 0;
-        int media = 0;
-        String nomeIdadeMenor = "";
-        String nomeIdadeMaior = "";
-
-        mostrarPessoas(nome, idade);
-        descobrirMenor(idade, maiorIdade, nomeIdadeMenor, nome);
-        descobrirMaior(idade,nomeIdadeMaior,nome);
-        System.out.printf("\n Quem tem a menor idade é %s com %d anos e quem tem a maior idade é %s com %d anos." +
-                nomeIdadeMenor, menorIdade, nomeIdadeMaior, maiorIdade);
-    }
-
+    int[] idade = {19, 20, 17, 25, 4};
+    String[] nome = {"José", "Jair", "Jaíne", "Julia", "Jordan"};
+    int menorIdade = 0;
+    int maiorIdade = 0;
+    int media = 0;
+    String nomeIdadeMenor = "";
+    String nomeIdadeMaior = "";
     public static void mostrarPessoas (String[] funcaoNome, int[] funcaoIdade){
         System.out.println("As pessoas desse grupo são: \n");
         for(int i = 0; i < 5; i ++){
